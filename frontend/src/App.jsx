76408e06acf5ada46a5f9366c15592c810d1f30b
Shirandu Warnakulasuriya <shirandushirandu@gmail.com>
2025-02-07 11:31:13 +0530

    do session 4 challenge.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
 import Navigation from "./Navigation.jsx"
 import Hero from "./Hero.jsx"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Calendar } from "@/components/ui/calendar"
 
 
 function App() {
@@ -11,6 +22,34 @@ function App() {
     <div> 
       <Navigation name={name} cartCount={cartCount}/>
       <Hero/>
+      <Card className="w-[350px] mx-auto border border-black">
+      <CardHeader>
+        <CardTitle>Card Component</CardTitle>
+        <CardDescription> Add Card component using shadcn/ui.</CardDescription>
+      </CardHeader>
+      <CardContent>
+      <div className="flex px-4 mt-4  items-center justify-between">
+        <h2 className="text-2xl  font-semibold"> Item 1 </h2>
+        <span className="block text-lg font-medium">$100.00 </span>
+      </div>
+      <div className="px-4 mt-2">
+        <p className="text-sm"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos
+        suscipit est autem quia? Voluptatem?</p>
+      </div>
+      <div className="mt-1 p-4">
+      </div>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline"> Later </Button>
+        <Button>Add to Cart</Button>
+      </CardFooter>
+    </Card>
+
+    <Calendar
+      mode="single"
+      className="rounded-md border"
+    />
+
     </div>
   );
 }
